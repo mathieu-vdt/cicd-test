099227b7482eb5e0a1cce2bd09899ef33370153a
TeyKz <81241850+TeyKz@users.noreply.github.com>
2024-08-27 11:24:20 +0200

    sum actions

---
+++ .github/workflows/sum.py
@@ -0,0 +1,12 @@
+# Déclaration des variables
+variable1 = 5
+variable2 = 10
+
+# Définition de la fonction add
+def add(a, b):
+    total = a + b
+    print(f"La somme de {a} et {b} est donc égal à ", total)
+    return total
+
+# Appel de la fonction add avec les variables
+somme = add(variable1, variable2)
